58428b243fb3a8eb53f175faa57b23c2cca385aa
Iamfaraz1 <farazt9400@gmail.com>
2024-11-16 18:29:42 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['fakestoreapi.com'], // Add 'fakestoreapi.com' to the domains
+      },
+};
+
+export default nextConfig;
